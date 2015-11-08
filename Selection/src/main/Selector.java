8642bb0336269efbe7d3aff1467aab62ef374d7e
Fernando Cristiani <fer5cristiani@gmail.com>
2015-11-08 13:29:02 -0300

    Implements Selection algorithm. Adds test with an array in ascending order

--- Selection/src/main/Selector.java
+++ Selection/src/main/Selector.java
@@ -1,9 +1,68 @@
 package main;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Selector {
 
 	public int selectOrderStatistic(int[] input, int orderStatistic) {
-		return input[0];
+		return doSelection(input,orderStatistic);
 	}
 
+	private int doSelection(int[] input, int orderStatistic) {
+		int inputLength = input.length;
+		if (inputLength == 1) {
+			return input[0];
+		}
+		
+		int pivot = choosePivot(input);
+		
+		swapValues(input, pivot, 0);
+		pivot = 0;
+		
+		pivot = partitionInputAroundPivot(input, pivot);
+		
+		if (pivot > transformOrderStatisticToArrayIndexes(orderStatistic)) {
+			int beforePivotPart[] = Arrays.copyOfRange(input, 0, pivot);
+			return doSelection(beforePivotPart, orderStatistic);
+		}
+		
+		if (pivot < transformOrderStatisticToArrayIndexes(orderStatistic)) {
+			int afterPivotPart[] = Arrays.copyOfRange(input, pivot + 1, inputLength);
+			return doSelection(afterPivotPart, transformOrderStatisticToArrayIndexes(orderStatistic) - pivot);
+		}
+		
+		return input[pivot];
+	}
+
+	private int transformOrderStatisticToArrayIndexes(int orderStatistic) {
+		return orderStatistic - 1;
+	}
+
+	private int choosePivot(int[] input) {
+		Random r = new Random();
+		return r.nextInt(input.length);
+	}
+	
+	private int partitionInputAroundPivot(int[] input, int pivot) {
+		int pivotValue = input[pivot];
+		int boundarySmallerBiggerThanPivot = pivot + 1;
+		for (int j = boundarySmallerBiggerThanPivot; j < input.length; j++) {
+			if (input[j] < pivotValue) {
+				swapValues(input, j, boundarySmallerBiggerThanPivot);
+				boundarySmallerBiggerThanPivot++;
+			}
+		}
+		
+		int newPivot = boundarySmallerBiggerThanPivot - 1;
+		swapValues(input, pivot, newPivot);
+
+		return newPivot;
+	}
+	
+	private void swapValues(int[] input, int i, int j) {
+		int tempValue = input[i];
+		input[i] = input[j];
+		input[j] = tempValue;
+	}
 }
--- Selection/src/test/SelectorTest.java
+++ Selection/src/test/SelectorTest.java
@@ -20,5 +20,28 @@ public class SelectorTest {
 		assertEquals(oneElementArrayUniqueValue, selector.selectOrderStatistic(oneElementArray, 3));
 		assertEquals(oneElementArrayUniqueValue, selector.selectOrderStatistic(oneElementArray, 10));
 	}
+	
+	@Test
+	public void whenTheInputArrayIsInAscendingOrderTheOrderStatisticAndThePositionOfTheArrayShouldBeEquivalent() {
+		Selector selector = new Selector();
 
+		int input[] = getArrayAscendingOrder(1000);
+		
+		int orderStatistic = 0;
+		for (int i = 0; i < input.length; i++) {
+			orderStatistic = i + 1;
+			assertEquals(input[i], selector.selectOrderStatistic(input, orderStatistic));
+		}
+	}
+	
+	private int[] getArrayAscendingOrder(int size) {
+		int[] arrayAscendingOrder = new int[size];
+
+		int j = 0;
+		for(int i = 0; i < arrayAscendingOrder.length ; i++) {
+			arrayAscendingOrder[i] = i + 1;
+		}
+
+		return arrayAscendingOrder;
+	}
 }
