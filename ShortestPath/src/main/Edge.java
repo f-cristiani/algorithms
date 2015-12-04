3c98662e12a61e2d7eda8922e6fcc319682ae2ec
Fernando Cristiani <fer5cristiani@gmail.com>
2015-12-03 21:48:39 -0300

    Adds pathLength parameter to Edge constructor

--- ShortestPath/src/main/Edge.java
+++ ShortestPath/src/main/Edge.java
@@ -1,13 +1,15 @@
 package main;
 
 public class Edge {
-	private Vertice tail;
-	private Vertice head;
+	private Vertex tail;
+	private Vertex head;
 	private int id;
+	private int pathLength;
 
-	public Edge(Vertice tail, Vertice head) {
+	public Edge(Vertex tail, Vertex head, int pathLength) {
 		this.tail = tail;
 		this.head = head;
+		this.pathLength = pathLength;
 	}
 	
 	public void setId(int id) {
@@ -18,23 +20,27 @@ public class Edge {
 		return this.id;
 	}
 	
-	public void setTail(Vertice tail) {
+	public void setTail(Vertex tail) {
 		this.tail = tail;
 	}
 	
-	public void setHead(Vertice head) {
+	public void setHead(Vertex head) {
 		this.head = head;
 	}
 	
-	public Vertice getTail() {
+	public Vertex getTail() {
 		return this.tail;
 	}
 	
-	public Vertice getHead() {
+	public Vertex getHead() {
 		return this.head;
 	}
 
 	public boolean isSelfLoop() {
 		return this.getTail().equals(this.getHead());
+	}
+
+	public Integer getLength() {
+		return this.pathLength;
 	}
 }
